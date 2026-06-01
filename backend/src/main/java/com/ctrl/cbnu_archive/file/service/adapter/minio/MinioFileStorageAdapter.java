77809709c42b35fb47f85ec341e6bb68851f3c94
Rahahhaaa <airlike7734@gmail.com>
2026-06-01 22:59:42 +0900

    [REFACTOR(backend)]: AWS S3 파일 스토리지를 MinIO로 교체

--- backend/src/main/java/com/ctrl/cbnu_archive/file/service/adapter/minio/MinioFileStorageAdapter.java
+++ backend/src/main/java/com/ctrl/cbnu_archive/file/service/adapter/minio/MinioFileStorageAdapter.java
@@ -1,39 +1,61 @@
-package com.ctrl.cbnu_archive.file.service.adapter.s3;
+package com.ctrl.cbnu_archive.file.service.adapter.minio;
 
 import com.ctrl.cbnu_archive.file.service.port.FileStoragePort;
 import java.io.InputStream;
+import java.net.URI;
 import java.time.Duration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
 
-@ConditionalOnProperty(prefix = "app.adapter", name = "storage", havingValue = "s3")
-public class S3FileStorageAdapter implements FileStoragePort {
+@Component
+@ConditionalOnProperty(prefix = "app.adapter", name = "storage", havingValue = "minio")
+public class MinioFileStorageAdapter implements FileStoragePort {
 
-    private static final Logger log = LoggerFactory.getLogger(S3FileStorageAdapter.class);
+    private static final Logger log = LoggerFactory.getLogger(MinioFileStorageAdapter.class);
 
     private final S3Client s3Client;
     private final S3Presigner presigner;
     private final String bucket;
 
-    public S3FileStorageAdapter(
-            @Value("${app.storage.s3.bucket}") String bucket,
-            @Value("${app.storage.s3.region:ap-northeast-2}") String region) {
+    public MinioFileStorageAdapter(
+            @Value("${app.storage.minio.endpoint}") String endpoint,
+            @Value("${app.storage.minio.bucket}") String bucket,
+            @Value("${app.storage.minio.access-key}") String accessKey,
+            @Value("${app.storage.minio.secret-key}") String secretKey) {
         this.bucket = bucket;
-        Region awsRegion = Region.of(region);
-        this.s3Client = S3Client.builder().region(awsRegion).build();
-        this.presigner = S3Presigner.builder().region(awsRegion).build();
-        log.info("[S3] FileStorageAdapter 초기화: bucket={}, region={}", bucket, region);
+        Region region = Region.US_EAST_1;
+        StaticCredentialsProvider credentials = StaticCredentialsProvider.create(
+                AwsBasicCredentials.create(accessKey, secretKey));
+        S3Configuration s3Config = S3Configuration.builder()
+                .pathStyleAccessEnabled(true)
+                .build();
+        URI endpointUri = URI.create(endpoint);
+        this.s3Client = S3Client.builder()
+                .region(region)
+                .endpointOverride(endpointUri)
+                .credentialsProvider(credentials)
+                .serviceConfiguration(s3Config)
+                .build();
+        this.presigner = S3Presigner.builder()
+                .region(region)
+                .endpointOverride(endpointUri)
+                .credentialsProvider(credentials)
+                .build();
+        log.info("[MinIO] FileStorageAdapter 초기화: endpoint={}, bucket={}", endpoint, bucket);
     }
 
     @Override
@@ -45,7 +67,7 @@ public class S3FileStorageAdapter implements FileStoragePort {
                 .contentLength(size)
                 .build();
         s3Client.putObject(request, RequestBody.fromInputStream(is, size));
-        log.info("[S3] upload 완료: key={}, size={}", path, size);
+        log.info("[MinIO] upload 완료: key={}, size={}", path, size);
         return path;
     }
 
@@ -55,7 +77,7 @@ public class S3FileStorageAdapter implements FileStoragePort {
                 .bucket(bucket)
                 .key(storedKey)
                 .build();
-        log.info("[S3] download 요청: key={}", storedKey);
+        log.info("[MinIO] download 요청: key={}", storedKey);
         return s3Client.getObject(request);
     }
 
@@ -66,7 +88,7 @@ public class S3FileStorageAdapter implements FileStoragePort {
                 .key(storedKey)
                 .build();
         s3Client.deleteObject(request);
-        log.info("[S3] delete 완료: key={}", storedKey);
+        log.info("[MinIO] delete 완료: key={}", storedKey);
     }
 
     @Override
@@ -76,7 +98,7 @@ public class S3FileStorageAdapter implements FileStoragePort {
                 .getObjectRequest(b -> b.bucket(bucket).key(storedKey))
                 .build();
         String url = presigner.presignGetObject(request).url().toString();
-        log.debug("[S3] presigned URL 생성: key={}, ttl={}s", storedKey, ttl.toSeconds());
+        log.debug("[MinIO] presigned URL 생성: key={}, ttl={}s", storedKey, ttl.toSeconds());
         return url;
     }
 }
